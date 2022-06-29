11ac4da31510425d93f3ee1bf58d608df168a53d
Tyler Bettilyon <teb@tebs-lab.com>
2022-06-29 16:08:37 -0600

    expanded the guessing game solution slightly.

--- 02-if-while-conditionals/03s-if-while-solution.py
+++ 02-if-while-conditionals/03s-if-while-solution.py
@@ -3,17 +3,23 @@ import random
 secret_number = random.randint(1, 100)
 number_of_guesses = 0
 
+print("Guess a number between 1 and 100")
+
 while True:
     print(f'\nYou have {10 - number_of_guesses} guesses remaining.')
     current_guess = input("Guess a number: ")
 
     # Check if the value can be coerced to an integer.
     if not current_guess.isnumeric():
-        print(f'You guessed {current_guess}, which is not an integer. Try again.')
+        print(f'You guessed {current_guess}, which is not a positive integer. Try again.')
         continue
 
     # Coerce the guess to an integer.
     current_guess = int(current_guess)
+
+    if 1 <= current_guess <= 100:
+        print("That number is out of the legal range, please guess between 1 and 100")
+        continue
 
     # Increment the number of number guesses.
     number_of_guesses += 1
@@ -22,7 +28,7 @@ while True:
         print("Congratulations, you guessed it!")
         break
     elif number_of_guesses == 10:
-        print("Unfortunately, you have run out of guesses. The number was {secret_number}.")
+        print(f"Unfortunately, you have run out of guesses. The number was {secret_number}.")
         break
     elif current_guess < secret_number:
         print(f'Your guess ({current_guess}) is smaller than the number I chose.')
